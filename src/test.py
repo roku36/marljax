eacc922c36abea84ffe884ff69c69a0338b6a4d1
roku36 <roku36000@gmail.com>
2023-11-21 17:20:59 +0900

    dap py

--- src/test.py
+++ src/test.py
@@ -1,3 +1,57 @@
-import marljax
+import marljax.ac as ac
+import jax
+import jax.numpy as jnp
+import flax.linen as nn
+import numpy as np
+import optax
+from flax.linen.initializers import constant, orthogonal
+from typing import Sequence, NamedTuple, Any
+from flax.training.train_state import TrainState
+import distrax
+from gymnax.wrappers.purerl import LogWrapper, FlattenObservationWrapper
+import jaxmarl
+from jaxmarl.wrappers.baselines import LogWrapper
+from jaxmarl.environments.overcooked import overcooked_layouts
+from jaxmarl.viz.overcooked_visualizer import OvercookedVisualizer
+import hydra
+from omegaconf import OmegaConf
 
-print(marljax.hello())
+# set hyperparameters:
+config = {
+    "LR": 2.5e-4,
+    "NUM_ENVS": 16,
+    "NUM_STEPS": 128,
+    "TOTAL_TIMESTEPS": 5e6,
+    "UPDATE_EPOCHS": 4,
+    "NUM_MINIBATCHES": 4,
+    "GAMMA": 0.99,
+    "GAE_LAMBDA": 0.95,
+    "CLIP_EPS": 0.2,
+    "ENT_COEF": 0.01,
+    "VF_COEF": 0.5,
+    "MAX_GRAD_NORM": 0.5,
+    "ACTIVATION": "tanh",
+    "ENV_NAME": "overcooked",
+    "ENV_KWARGS": {
+      "layout" : "cramped_room"
+    },
+    "ANNEAL_LR": True,
+    "SEED": 0,
+    "NUM_SEEDS": 3
+}
+
+config["ENV_KWARGS"]["layout"] = overcooked_layouts[config["ENV_KWARGS"]["layout"]]
+rng = jax.random.PRNGKey(config["SEED"])
+print("rng", rng)
+rngs = jax.random.split(rng, config["NUM_SEEDS"])
+print("rngs", rngs)
+with jax.disable_jit(False):
+    train_jit = jax.jit(jax.vmap(ac.make_train(config)))
+    out = train_jit(rngs)
+
+
+for i in range(config["NUM_SEEDS"]):
+    plt.plot(out["metrics"]["returned_episode_returns"][i].mean(-1).reshape(-1))
+plt.xlabel("Update Step")
+plt.ylabel("Return")
+plt.show()
